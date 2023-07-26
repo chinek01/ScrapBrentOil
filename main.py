7683a053c45076fab48a1e6cce6da890ec4ffcca
MarcinC <chinek01@gmail.com>
2023-07-26 10:17:20 +0200

    init project

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+
+Portfolio: ScrapBrentOil
+#100DaysOfCode with Python
+Day: 91
+Date: 2023-07-23
+Author: MC
+
+"""
+
+
+
+# example url
+url = "https://www.eia.gov/dnav/pet/hist/LeafHandler.ashx?n=PET&s=RBRTE&f=M"
+
